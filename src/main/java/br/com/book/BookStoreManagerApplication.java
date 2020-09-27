ae4a44f07786d1b9089abc6fd2f617fb93bda84f
Gilson Silva <gilsonsilvati@gmail.com>
2020-09-27 16:34:44 -0300

    Developed project entities

--- src/main/java/br/com/book/BookStoreManagerApplication.java
+++ src/main/java/br/com/book/BookStoreManagerApplication.java
@@ -1,4 +1,4 @@
-package br.com.book.bookstoremanager;
+package br.com.book;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/br/com/book/BookStoreManagerApplicationTests.java
+++ src/test/java/br/com/book/BookStoreManagerApplicationTests.java
@@ -1,4 +1,4 @@
-package br.com.book.bookstoremanager;
+package br.com.book;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
