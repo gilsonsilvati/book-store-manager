9408cd51c1c3a1fd5e45a01f98f77c6e2a0ef1a3
Gilson Silva <gilsonsilvati@gmail.com>
2020-09-27 22:24:00 -0300

    Developed get by id operation

--- src/main/java/br/com/book/resource/BookResource.java
+++ src/main/java/br/com/book/resource/BookResource.java
@@ -4,10 +4,7 @@ import br.com.book.dto.BookDTO;
 import br.com.book.dto.MessageResponseDTO;
 import br.com.book.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -21,6 +18,11 @@ public class BookResource {
     @PostMapping
     public MessageResponseDTO create(@RequestBody @Valid BookDTO bookDTO) {
         return bookService.create(bookDTO);
+    }
+
+    @GetMapping("/{id}")
+    public BookDTO findById(@PathVariable Long id) {
+        return bookService.findById(id);
     }
 
 }
--- src/main/java/br/com/book/service/BookService.java
+++ src/main/java/br/com/book/service/BookService.java
@@ -8,6 +8,8 @@ import br.com.book.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class BookService {
 
@@ -25,4 +27,8 @@ public class BookService {
                 .build();
     }
 
+    public BookDTO findById(Long id) {
+        Optional<Book> optionalBook = bookRepository.findById(id);
+        return bookMapper.toDTO(optionalBook.get());
+    }
 }
